f72b02122cf91c3232c5532e4869b3e83e869285
Rares75 <rarescabalau62@gmail.com>
2026-01-04 23:59:53 +0200

    adding data base model

---
+++ project/tpaste-server/src/db_model.rs
@@ -0,0 +1,28 @@
+use chrono::{DateTime, Local, NaiveDateTime, Utc};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    pub id: Option<i64>,
+    pub username: String,
+    pub password_hash: String,
+    pub created_at: DateTime<Utc>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Paste {
+    pub id: Option<i64>,
+    pub user_id: i64,
+    pub code: String,
+    pub content: String,
+    pub created_at: DateTime<Utc>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Token {
+    pub id: Option<i64>,
+    pub user_id: i64,
+    pub token: String,
+    pub created_at: DateTime<Utc>,
+    pub expires_at: DateTime<Utc>, //+60 days
+}
